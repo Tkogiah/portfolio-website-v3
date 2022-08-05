910072deeeb94b144642eae370159ca6f1e7da6d
Marcus Overstreet <tkogiah@gmail.com>
2022-08-05 03:39:58 -0700

    added netlify link to projects object

--- src/pages/ProjectList.js
+++ src/pages/ProjectList.js
@@ -4,7 +4,7 @@ export const projectList = [
         title: 'Upwork Clone',
         description: 'This was a project I built that helped me to play with and learn React to a point where I feel comfortable freely exploring and building in React. This project was designed to help me get used to using material components, react router, and one way data flow in React.',
         video: [],
-        website: '',
+        website: 'https://warm-bavarois-ec9384.netlify.app/',
         github: 'https://github.com/Tkogiah/upwork-clone',
         tags: [
             {
@@ -30,7 +30,7 @@ export const projectList = [
         title: 'Crypto API App',
         description: 'This project was focused around a real world application where I took a Crypto API and filtered it by the 5 coins I wanted to keep track of in their daily rate of change. Learning how to do API calls with React was the primary focus of this project',
         video: [],
-        website: '',
+        website: 'https://voluble-starlight-952012.netlify.app/',
         github: 'https://github.com/Tkogiah/crypto-api-app',
         tags: [
             {
@@ -134,7 +134,7 @@ export const projectList = [
         title: 'Pexels API',
         description: 'This was my last project in learning the fundamentals of Javascript. I struggled to understand API calls and so I built this project to gain a deeper grasp on how authentication headers work. Pexels has an open API that you just need to request a key for. This project was what made me feel like I was starting to understand web development.',
         video: ['https://www.youtube.com/embed/2svY-fFoWPQ', 'https://youtu.be/2svY-fFoWPQ'],
-        website: '',
+        website: 'https://fancy-puffpuff-b20cb4.netlify.app/',
         github: 'https://github.com/Tkogiah/pexels-api',
         tags: [
 
