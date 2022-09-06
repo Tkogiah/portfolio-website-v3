319881f56c87e5ddd1ab4ffb7dc600ccb84b81bc
Marcus Overstreet <tkogiah@gmail.com>
2022-09-06 07:59:31 -0700

    added another project tab

--- src/pages/ProjectList.js
+++ src/pages/ProjectList.js
@@ -1,6 +1,28 @@
 export const projectList = [
     {
-        id: 0,
+        id: 7,
+        title: 'My BoardGame',
+        description: "Back in 2014 I made a boardgame and when I was all finished I only ever had one complaint from people `there's too much math`. So now that I know how to code I decided to build this. No this isn't done but it is a working version. This project taught me so much about project management, object oriented programming, algorithms and so many other things. I had so much fun and I even made the music for this!!!",
+        video: [],
+        website: 'https://superb-custard-5d403a.netlify.app/',
+        github: 'https://github.com/Tkogiah/test-game',
+        tags: [
+            {
+                title: 'Java Script',
+                icon: 'js.png'
+            },
+            {
+                title: 'SCSS',
+                icon: 'sass.png'
+            },
+            {
+                title: 'HTML',
+                icon: 'html-5.png'
+            }
+        ]
+    },
+    {
+        id: 6,
         title: 'My personal website',
         description: "My original website marcusoverstreet.com was built with pagefly but after becoming comfortable with React I wanted to try and build one myself. It isn't perfect and I have many little bells and whistles I will be adding to it. This was my first attempt using parallax pages. The biggest struggle was fixing the client side rendering on refresh.",
         video: ['https://www.youtube.com/embed/H1h8ez1OtsE', 'https://youtu.be/H1h8ez1OtsE'],
@@ -26,7 +48,7 @@ export const projectList = [
         ]
     },
     // {   
-    //     id: 1,
+    //     id: 5,
     //     title: 'Browser Router website',
     //     description: '(NOT MOBILE FRIENDLY YET) This was a project I built that helped me to play with and learn React to a point where I feel comfortable freely exploring and building in React. This project was designed to help me get used to using material components, react router, and one way data flow in React.',
     //     video: ['https://www.youtube.com/embed/TheRn_YUgtw', 'https://youtu.be/TheRn_YUgtw'],
@@ -52,7 +74,7 @@ export const projectList = [
     //     ]
     // },
     {
-        id: 2,
+        id: 4,
         title: 'Crypto API App',
         description: 'This project was focused around a real world application where I took a Crypto API and filtered it by the 5 coins I wanted to keep track of in their daily rate of change. Learning how to do API calls with React was the primary focus of this project',
         video: [],
@@ -104,7 +126,7 @@ export const projectList = [
         ]
     },
     {
-        id: 4,
+        id: 2,
         title: 'Tomato Counter',
         description: 'This was a project to get familiar with the UseState hook in React. Updating only one day but maintaning the value of each day was a greay intro into how the hook works. ',
         video: [],
@@ -130,7 +152,7 @@ export const projectList = [
         ]
     },
     {
-        id: 5,
+        id: 1,
         title: 'React Landing Page',
         description: 'This was my first project with React. I was just getting familiar with styling and components and how to organize projects in React.',
         video: [],
@@ -156,7 +178,7 @@ export const projectList = [
         ]
     },
     {
-        id: 6,
+        id: 0,
         title: 'Pexels API',
         description: 'This was my last project in learning the fundamentals of Javascript. I struggled to understand API calls and so I built this project to gain a deeper grasp on how authentication headers work. Pexels has an open API that you just need to request a key for. This project was what made me feel like I was starting to understand web development.',
         video: ['https://www.youtube.com/embed/2svY-fFoWPQ', 'https://youtu.be/2svY-fFoWPQ'],
